4e24ceed88561c381bfac3fc1f66bfb70bf96857
Fatih Çetiner <f-cetiner99@hotmail.com>
2023-12-25 20:54:48 +0300

    Date Pattern

--- app/src/main/java/com/example/rickandmorty/utils/Constants.kt
+++ app/src/main/java/com/example/rickandmorty/utils/Constants.kt
@@ -4,4 +4,6 @@ object Constants {
 
     const val BASE_URL: String = "https://rickandmortyapi.com/api/"
     const val END_POINT: String = "character"
+
+    const val DATE_PATTERN: String = "d MMMM yyyy, HH:mm:ss"
 }
--- app/src/main/java/com/example/rickandmorty/utils/DateConverter.kt
+++ app/src/main/java/com/example/rickandmorty/utils/DateConverter.kt
@@ -2,6 +2,7 @@ package com.example.rickandmorty.utils
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import com.example.rickandmorty.utils.Constants.DATE_PATTERN
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
@@ -10,7 +11,7 @@ class DateConverter {
         @RequiresApi(Build.VERSION_CODES.O)
         fun convertDate(date: String): String {
             val date = LocalDateTime.parse(date, DateTimeFormatter.ISO_DATE_TIME)
-            val formattedDate = date.format(DateTimeFormatter.ofPattern("d MMMM yyyy, HH:mm:ss"))
+            val formattedDate = date.format(DateTimeFormatter.ofPattern(DATE_PATTERN))
             return formattedDate.toString()
         }
     }
